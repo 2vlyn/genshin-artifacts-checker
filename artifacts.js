5e3849c0034f0fc662d4f06b404c41efb254d94e
2vlyn <rty1000y@gmail.com>
2025-02-15 21:04:15 +0900

    Update artifacts.js

--- artifacts.js
+++ artifacts.js
@@ -30,14 +30,16 @@ function get_artifactsInfo(artifactsName) {
                     const data = results.data;
                     const targetRow = data.find(row => row.name === artifactsName);
                     if (targetRow) {
-                        document.write(JSON.stringify(targetRow, null, 2));
+                        return JSON.stringify(targetRow, null, 2);
                     } else {
                         console.log('해당 성유물을 찾을 수 없습니다.');
+                        return null;
                     }
                 }
             });
         })
         .catch(error => {
             console.error('CSV 파일을 읽는 중 오류 발생:', error);
+            return null;
         });
 }
