b69dc06ee5ae27f5d9f120781b6f47eaf20df5b9
2vlyn <rty1000y@gmail.com>
2025-02-15 20:40:26 +0900

    Update artifacts.js

--- artifacts.js
+++ artifacts.js
@@ -1,4 +1,22 @@
 function get_artifactsList() {
+    fetch('data/artifacts.csv')
+        .then(response => response.text())
+        .then(csvText => {
+            Papa.parse(csvText, {
+                header: true,
+                complete: function(results) {
+                    const data = results.data;
+                    if (data.length > 0) {
+                        return JSON.stringify(data, null, 2);
+                    } else {
+                        console.log('데이터가 없습니다.');
+                    }
+                }
+            });
+        })
+        .catch(error => {
+            console.error('CSV 파일을 읽는 중 오류 발생:', error);
+        });
 }
 
 
